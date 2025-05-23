3997131e1f1c0f0d15349679ffae7263ae7290e6
hutechnang <160829545+hutechnang@users.noreply.github.com>
2025-05-24 00:01:22 +0700

    Xoá file spam

--- README.md
+++
@@ -1,2 +0,0 @@
-# bmttnc-hutech-2280602045
-Đinh Ngọc Năng_2280602045
--- ex02_10.py
+++
@@ -1,7 +0,0 @@
-def dao_nguoc_chuoi(chuoi):
-    return chuoi[::-1]
-
-# Sử dụng hàm và in kết quả
-input_string = input("Mời nhập chuỗi cần đảo ngược: ")
-chuoi_dao_nguoc = dao_nguoc_chuoi(input_string)
-print("Chuỗi đảo ngược là:", chuoi_dao_nguoc)
